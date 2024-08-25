15bb46e6709b815b0715cb120b4b846de6be6f19
Bharat <bharat18.tech@gmail.com>
2024-08-25 23:45:06 +0530

    R-2.1.4 | Use Javascript in JSX

--- App.js
+++ App.js
@@ -1,17 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React Component
-const Title = () => (
+const elem = <span>React Element</span>
+
+// React Element
+const title = (
     <h1 className="head">
+        {elem}
         Namaste React using JSX
     </h1>
 )
 
+const number = 10000;
+
 // Component Composition
 const HeadingComponent = () => (
     <div id="container">
-        <Title/>
+        <h2>{100+200}</h2>
+        <br/>
+        {number}
+        <br/>
+        {title}
         <h1 className="heading">Heading Functional Component</h1>
     </div>
 );
