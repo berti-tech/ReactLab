a30346d97bfd7d89872727f93aefdefa2a51f655
Bharat <bharat18.tech@gmail.com>
2024-08-28 15:40:01 +0530

    R-3.0.0 | Initial commit: Add basic structure for restaurant app
    - Implemented header component
    - Added body section with RestaurantCard component

--- App.js
+++ App.js
@@ -1,31 +1,103 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import AppLogo from "./assets/AppLogo.png";
 
-const elem = <span>React Element</span>
+/**
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * 
+ * Body
+ *  - Search
+ *  - Restaurant Container
+ *      - RestaurantCard
+ *          - Img
+ *          - Name
+ *          - Cuisines
+ *          - Star Rating
+ *          - Delivery Time
+ * 
+ * Footer
+ *  - Copyright
+ *  - Links
+ *  - Address
+ *  - Contact
+ * 
+ */
 
-// React Element
-const title = (
-    <h1 className="head">
-        {elem}
-        Namaste React using JSX
-    </h1>
-)
 
-const number = 10000;
+// Header component -> Logo & Nav Items
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="logo-container">
+                <img className="logo" src={AppLogo} draggable="false"/>
+            </div>
+            <div className="nav-items">
+                <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Cart</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
 
-// Component Composition
-const HeadingComponent = () => (
-    <div id="container">
-        <h2>{100+200}</h2>
-        <br/>
-        {number}
-        <br/>
-        {title}
-        <h1 className="heading">Heading Functional Component</h1>
-    </div>
-);
+const styleCard = {
+    backgroundColor: "#f0f0f0"
+}
+
+const RestaurantCard = () => {
+    return (
+        <div className="res-card" style={styleCard}>
+            <img className="res-logo"
+                alt="res-logo" 
+                src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/nw2xeql0jokm2rqnkzfs"/>
+            <h3>Meghna Foods</h3>
+            <h4>Biryani, North Indian, Asian</h4>
+            <h4>Rating : 4.4</h4>
+            <h4>38 min.</h4>
+
+        </div>
+    )
+}
+
+const Body = () => {
+    return (
+        <div className="body">
+            <div className="search">
+                Search
+            </div>
+            <div className="restaurant-container">
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+                <RestaurantCard/>
+            </div>
+        </div>
+    )
+}
+
+// Functional Component is a normal function that returns some piece of JSX code
+// Conatiner div
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // Rendering a component
-root.render(<HeadingComponent/>)
+root.render(<AppLayout/>)
