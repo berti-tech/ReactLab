bdf6d08756fc3fe0cf01a7b2a452642f4ff27988
Bharat <bharat18.tech@gmail.com>
2024-09-04 00:45:32 +0530

    R-6.1 | Create Children Routes in React

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Body from "./Components/Body";
 import About from "./Components/About";
 import Contact from "./Components/Contact"
 import Error from "./Components/Error"
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet} from "react-router-dom";
 
 
 // Functional Component is a normal function that returns some piece of JSX code
@@ -13,34 +13,40 @@ const AppLayout = () => {
     return (
         <div className="app">
             <Header/>
-            <Body/>
+            <Outlet/>
         </div>
     )
 }
 
 /**
- * Creating Routes in React
+ * Create Children Routes in React
  */
 
-// Create routing configurations in App.js to implement routes
-// createBrowserRouter will create a routing configuration for us.
-// We are creating routing configuration inside appRouter
+// Implement Children Routes
+// If I go to About Us Page, I loose my header. 
+// Ideally the header should always be on the top of the app and the About Us page should load below.
+// Solution is to create children routes
 const appRouter = createBrowserRouter([
     {
         path: "/",
         element: <AppLayout/>,
+        children: [
+            {
+                path: "/",
+                element: <Body/>
+            },
+            {
+                path: "/about",
+                element: <About/>
+            },
+            {
+                path: "/contact",
+                element: <Contact/>
+            },
+        ],
         errorElement: <Error/>
     },
-    {
-        path: "/about",
-        element: <About/>
-    },
-    {
-        path: "/contact",
-        element: <Contact/>
-    },
-
-])
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import AppLogo from "../utils/AppLogo.png";
 import {useState} from "react"
+import {Link} from "react-router-dom"
 
 // Create toggle Button for Login-Logout
 // Whenever the state variable changes, React will render the header component
@@ -14,10 +15,18 @@ const Header = () => {
             </div>
             <div className="nav-items">
                 <ul>
-                    <li>Home</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                    <li>Cart</li>
+                    <li>
+                        <Link>Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About Us</Link>
+                    </li>
+                    <li>
+                        <Link to="/contact">Contact Us</Link>
+                    </li>
+                    <li>
+                        Cart
+                    </li>
                     <button className="login" onClick={()=>{
                         btnName === "Login" ? setBtnName("Logout") : setBtnName("Login")
                     }}>{btnName}</button>
