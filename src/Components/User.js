f8221b800415357774e140a99352bfc80693a06d
Bharat <bharat18.tech@gmail.com>
2024-09-08 11:36:26 +0530

    R-7.1 | Create multiple state variables in class components

--- src/Components/User.js
+++ src/Components/User.js
@@ -1,7 +1,14 @@
+import {useState} from "react";
+
 
 const User = (props) => {
+    const [count] = useState(0);
+    const [count2] = useState(1);
+    console.log(count);
     return (
         <div className="user-card">
+            <h3>Count: {count}</h3>
+            <h3>Count2: {count2}</h3>
             <h3>Name: {props.name}</h3>
             <h3>Location: {props.location}</h3>
             <h4>Contact: @bharat18</h4>
--- src/Components/UserClass.js
+++ src/Components/UserClass.js
@@ -2,11 +2,23 @@ import React from "react";
 
 // Class based component
 
-// render() method will return some piece of JSX which will be displayed to the UI
+
+/**
+ * Goal : 
+ *   1. How to create state variables in class based components
+ *   2. Creating multiple state variables in class based components
+ */
+
+// State is created in class components whenever we create a instance of the class
+
 class UserClass extends React.Component {
 
     constructor(props) {
         super(props);
+        this.state = {
+            count: 0,
+            count2: 2,
+        };
 
         console.log(props);
     }
@@ -14,6 +26,8 @@ class UserClass extends React.Component {
 
         return (
             <div className="user-card">
+                <h3>Count: {this.state.count}</h3>
+                <h3>Count2: {this.state.count2}</h3>
                 <h3>Name: {this.props.name}</h3>
                 <h3>Location: {this.props.location}</h3>
                 <h4>Contact: @bharat18</h4>
